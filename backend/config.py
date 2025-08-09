a9ee2a1c8b14301b5729a7d998a4d53357fe7bfa
Nguyễn Công Trình <nctrinh2705@gmail.com>
2025-08-09 23:56:04 +0700

    refactor: move config.setting content to config.py

--- backend/config.py
+++ backend/config.py
@@ -3,4 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./data/app.db")
+
+
+RAW_DIR = os.path.join(BASE_DIR, 'data', 'raw')
+RECIPES_FOR_CHROMA_DIR = os.path.join(BASE_DIR, 'data', 'processed')
+CHROMA_DB_DIR = os.path.join(BASE_DIR, 'data', 'chroma_db')
